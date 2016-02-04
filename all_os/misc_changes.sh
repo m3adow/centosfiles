549900540d5424d7189cbfadc98f045b12f0aaaf
Till Wiese <mail-github@till-wiese.de>
2016-02-04 14:08:49 +0100

    HISTSIZE 5000 > 1000

--- all_os/misc_changes.sh
+++ all_os/misc_changes.sh
@@ -4,4 +4,4 @@ set -u
 
 # This is most likely usable for all OS versions, although it's not always the best practice
 echo "export HISTTIMEFORMAT='%F %T '" >> /etc/profile
-echo "export HISTSIZE=1000" >> /etc/profile
+echo "export HISTSIZE=5000" >> /etc/profile
