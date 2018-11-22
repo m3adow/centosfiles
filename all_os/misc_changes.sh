af312d077e86de8551ad76b6f6d6b52622835ab8
Till Wiese <mail-github@till-wiese.de>
2018-11-22 16:44:18 +0100

    Update misc_changes.sh

--- all_os/misc_changes.sh
+++ all_os/misc_changes.sh
@@ -3,6 +3,7 @@ set -e
 set -u
 
 # This is most likely usable for all OS versions, although it's not always the best practice
+echo "shopt -s histappend" >> /etc/profile
 echo "export HISTTIMEFORMAT='%F %T '" >> /etc/profile
 echo "export HISTSIZE=10000" >> /etc/profile
 echo "export HISTCONTROL='ignoredups'" >> /etc/profile
