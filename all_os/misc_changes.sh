3742a51b12a0a64e7cca849727722ccc25472e4c
Till Wiese <1872304+m3adow@users.noreply.github.com>
2019-10-25 10:23:09 +0200

    Update misc_changes.sh

--- all_os/misc_changes.sh
+++ all_os/misc_changes.sh
@@ -5,6 +5,7 @@ set -u
 # This is most likely usable for all OS versions, although it's not always the best practice
 echo "shopt -s histappend" >> /etc/profile
 echo "export HISTTIMEFORMAT='%F %T '" >> /etc/profile
+echo "export HISTFILESIZE=1000000" >> /etc/profile
 echo "export HISTSIZE=10000" >> /etc/profile
-echo "export HISTCONTROL='ignoredups'" >> /etc/profile
-echo "export HISTIGNORE='ls:ps'" >> /etc/profile
+echo "export HISTCONTROL='ignoreboth:erasedups'" >> /etc/profile
+echo "export HISTIGNORE='ls:ps:ll:history'" >> /etc/profile
